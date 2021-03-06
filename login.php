90e3179873fba54050ab3788691f018fbc43b937
ulrich01 <victor.demo2008@gmail.com>
2021-03-06 22:49:41 +0100

    Authentication-sys

---
+++ login.php
@@ -0,0 +1,54 @@
+<?php session_start(); /* Starts the session */
+/* Check Login form submitted */if(isset($_POST['Submit'])){
+/* Define username and associated password array */$logins = array('Admin' => 'admin1234','username1' => 'password1','username2' => 'password2');
+
+/* Check and assign submitted Username and Password to new variable */$Username = isset($_POST['Username']) ? $_POST['Username'] : '';
+$Password = isset($_POST['Password']) ? $_POST['Password'] : '';
+
+/* Check Username and Password existence in defined array */if (isset($logins[$Username]) && $logins[$Username] == $Password){
+/* Success: Set session variables and redirect to Protected page  */$_SESSION['UserData']['Username']=$logins[$Username];
+header("location:index.php");
+exit;
+} else {
+/*Unsuccessful attempt: Set error message */$msg="<span style='color:red'>Invalid Login Details</span>";
+}
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+
+<form action="" method="post" name="Login_Form">
+  <table width="400" border="0" align="center" cellpadding="5" cellspacing="1" class="Table">
+    <?php if(isset($msg)){?>
+    <tr>
+      <td colspan="2" align="center" valign="top"><?php echo $msg;?></td>
+    </tr>
+    <?php } ?>
+    <tr>
+      <td colspan="2" align="left" valign="top" style="text-align: center"><h3>Login</h3></td>
+    </tr>
+    <tr>
+      <td align="right" valign="top">Username</td>
+      <td><input name="Username" type="text" class="Input"></td>
+    </tr>
+    <tr>
+      <td align="right">Password</td>
+      <td><input name="Password" type="password" class="Input"></td>
+    </tr>
+    <tr>
+      <td> </td>
+      <td><input name="Submit" type="submit" value="Login" style= "background: blue" class="Button3"></td>
+    </tr>
+  </table>
+</form>
+<p style= "text-align: center">Username: Admin <br> Password: admin1234</p>
+
+</body>
+</html>
